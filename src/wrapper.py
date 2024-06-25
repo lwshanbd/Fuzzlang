aec573ed7907c6162ce69274443f74b7ff65bf35
Baodi Shan <shan4@llnl.gov>
2024-06-24 17:29:46 -0700

    Update reordering

--- src/wrapper.py
+++ src/wrapper.py
@@ -1,12 +1,80 @@
-#!/usr/bin/python
+#!/usr/tce/packages/python/python-3.9.12/bin/python
 
 """This script wraps the compiler, fuzzes the command line arguments, and executes the compiler with the fuzzed arguments."""
 
 import subprocess
 import sys
 import random
+import json
+import os
+from datetime import datetime
 
 RECOGNIZED_SOURCE_FILE_EXTENSIONS = ['.c', '.cpp', '.cxx', '.cc', '.c++']
+log_file_path = '/p/lustre3/shan4/Fuzzlang/error_log1.json'
+fuzz_modes = []
+remove_level = 0
+
+# Fuzz the command line arguments based on the FUZZ_MODE environment
+# Fuzz mode:
+#  1 - reorder: Randomly reorder the arguments
+#  2 - remove: Randomly remove a subset of the arguments
+#  3 - replace: Randomly replace a subset of the arguments
+#  4 - insert: Randomly insert a subset of the arguments
+# Remove level:
+#  1 - low: Remove 1 of the arguments
+#  2 - medium: Remove 1 to 1/2 of the arguments
+#  3 - high: Remove 1/2 to all of the arguments
+def parse_fuzz_mode():
+    # Get the FUZZ_MODE environment variable
+    fuzz_mode = os.getenv('FUZZ_MODE', '')
+    remove_level = os.getenv('REMOVE_LEVEL', '')
+    
+    # Print the original FUZZ_MODE value
+    print(f"Original FUZZ_MODE: {fuzz_mode}")
+    
+    # Handle different separators & and ,
+    separators = [',', '&']
+    for sep in separators:
+        if sep in fuzz_mode:
+            fuzz_mode = fuzz_mode.replace(sep, ' ')
+    
+    # Split the string into individual modes
+    modes = fuzz_mode.split()
+    
+    # Remove any potential empty strings and duplicates
+    modes = list(set([mode.strip() for mode in modes if mode.strip()]))
+    for mode in modes:
+        if mode == 'reorder':
+            fuzz_modes.append('reorder')
+        elif mode == 'remove':
+            fuzz_modes.append('remove')
+        elif mode == 'replace':
+            fuzz_modes.append('replace')
+        elif mode == 'insert':
+            fuzz_modes.append('insert')
+        else:
+            print(f"Invalid fuzz mode: {mode}")
+    if remove_level:
+        try:
+            remove_level = int(remove_level)
+        except ValueError:
+            print(f"Invalid remove level: {remove_level}")
+            print("Remove level must be an integer value between 1 and 3")
+
+
+def log_to_json(fuzzed_args, original_command, new_command_line, status, message):
+    log_entry = {
+        "timestamp": datetime.now().isoformat(),
+        "fuzzed_args": fuzzed_args,
+        "original_command": original_command,  # Convert list to string for logging
+        "fuzzed_command": new_command_line,
+        "status": status,
+        "message": message
+    }
+    
+    with open(log_file_path, 'a') as log_file:
+        json.dump(log_entry, log_file)
+        log_file.write('\n')  # Add newline for readability
 
 
 def recognize_source_file_extension(file_path):
@@ -16,36 +84,73 @@ def recognize_source_file_extension(file_path):
 
     return False
 
-
-def fuzz_clang_runner(command_line):
-
+def fuzz_remove(command_line):
     args_to_fuzz = command_line['other_params']
 
     # Randomly determine the number of arguments to remove
-    num_to_remove = random.randint(0, len(args_to_fuzz))
+    if remove_level == 1:
+        num_to_remove = 1
+    elif remove_level == 2:
+        num_to_remove = random.randint(1, int(len(args_to_fuzz)/2))
+    elif remove_level == 3:
+        num_to_remove = random.randint(int(len(args_to_fuzz)/2), len(args_to_fuzz))
+    else:
+        num_to_remove = random.randint(1, len(args_to_fuzz))    
     fuzzed_args = random.sample(args_to_fuzz, num_to_remove)
 
     # Create a new list of arguments excluding the randomly selected ones
-    other_params = [arg for arg in args_to_fuzz if arg not in fuzzed_args]
+    fuzzed_args = [arg for arg in args_to_fuzz if arg not in fuzzed_args]
 
     # Build the new command line
     new_command_line = []
     new_command_line.append(command_line['compiler'])
-    new_command_line.extend(command_line['source_file'])
+    if command_line['source_file']:
+        new_command_line.extend(command_line['source_file'])
     if command_line['output_flag']:
         new_command_line.append(command_line['output_flag'])
         new_command_line.append(command_line['output_file'])
-    new_command_line.extend(other_params)
-
+    backup_command_line = new_command_line.copy()
+    backup_command_line.extend(args_to_fuzz)
+    new_command_line.extend(fuzzed_args)
+    
     # Execute the new command line
     try:
         result = subprocess.run(
             new_command_line, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("SUCCESS:", " ".join(new_command_line))
-        print("Output:", result.stdout.decode())
+        log_to_json(fuzzed_args, backup_command_line, new_command_line, "SUCCESS", result.stdout.decode().strip())
     except subprocess.CalledProcessError as e:
-        print("FAILURE:", " ".join(new_command_line))
-        print("Error:", e.stderr.decode())
+        error_message = e.stderr.decode()
+        log_to_json(fuzzed_args, backup_command_line, new_command_line, "ERROR", error_message)
+        subprocess.run(backup_command_line, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+def fuzz_reordering(command_line):
+    args_to_fuzz = command_line['other_params']
+
+    # Randomly shuffle the arguments
+    random.shuffle(args_to_fuzz)
+
+    # Build the new command line
+    new_command_line = []
+    new_command_line.append(command_line['compiler'])
+    if command_line['source_file']:
+        new_command_line.extend(command_line['source_file'])
+    if command_line['output_flag']:
+        new_command_line.append(command_line['output_flag'])
+        new_command_line.append(command_line['output_file'])
+    backup_command_line = new_command_line.copy()
+    backup_command_line.extend(args_to_fuzz)
+    new_command_line.extend(args_to_fuzz)
+    
+    # Execute the new command line
+    try:
+        result = subprocess.run(
+            new_command_line, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        log_to_json(None, backup_command_line, new_command_line, "SUCCESS", result.stdout.decode().strip())
+    except subprocess.CalledProcessError as e:
+        error_message = e.stderr.decode()
+        log_to_json(None, backup_command_line, new_command_line, "ERROR", error_message)
+        subprocess.run(backup_command_line, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 def parse_clang_command(command_line):
@@ -86,12 +191,11 @@ def parse_clang_command(command_line):
 # Example usage
 if __name__ == "__main__":
     command = sys.argv
-    print("Command Line: ", command)
+    parse_fuzz_mode()
     parsed_command = parse_clang_command(command)
-    print("Parsed Command Components:")
-    print("Compiler: ", parsed_command['compiler'])
-    print("Source File: ", parsed_command['source_file'])
-    print("Output Flag: ", parsed_command['output_flag'])
-    print("Output File: ", parsed_command['output_file'])
-    print("Other Parameters: ", ' '.join(parsed_command['other_params']))
-    fuzz_clang_runner(parsed_command)
+    fuzz_mode = random.choice(fuzz_modes)
+    if fuzz_mode == 'reorder':
+        fuzz_reordering(parsed_command)
+    elif fuzz_mode == 'remove':
+        fuzz_remove(parsed_command)
+    
